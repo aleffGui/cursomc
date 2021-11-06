b50c9e64a56436c025692dabddcfdfb2ad9f1f29
Guilherme Ferreira <alefff.guilherme@gmail.com>
2021-11-06 10:27:54 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/guilherme/cursomc/CursomcApplication.java
+++ src/main/java/com/guilherme/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.guilherme.cursomc.domain.Categoria;
 import com.guilherme.cursomc.domain.Cidade;
+import com.guilherme.cursomc.domain.Cliente;
+import com.guilherme.cursomc.domain.Endereco;
 import com.guilherme.cursomc.domain.Estado;
 import com.guilherme.cursomc.domain.Produto;
+import com.guilherme.cursomc.enums.TipoCliente;
 import com.guilherme.cursomc.repositories.CategoriaRepository;
 import com.guilherme.cursomc.repositories.CidadeRepository;
+import com.guilherme.cursomc.repositories.ClienteRepository;
+import com.guilherme.cursomc.repositories.EnderecoRepository;
 import com.guilherme.cursomc.repositories.EstadoRepository;
 import com.guilherme.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,13 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	EstadoRepository estadoRepository;
+	
+	@Autowired
+	EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -68,6 +80,17 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
+		Cliente cli1 = new Cliente(null, "Guilherme Ferreira", "guilherme@gmail.com", "43947557457", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("4233054785", "42998474587"));
+		
+		Endereco end1 = new Endereco(null, "Rua Flores","300", "Apto 303", "Jardim", "85040540", cli1, c1);
+		Endereco end2 = new Endereco(null, "Avenida Matos","105", "Sala 800", "Centro", "81050250", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(end1,end2));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(end1,end2));
+		
 		
 	}
 
--- src/main/java/com/guilherme/cursomc/domain/Cidade.java
+++ src/main/java/com/guilherme/cursomc/domain/Cidade.java
@@ -1,5 +1,7 @@
 package com.guilherme.cursomc.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,7 +9,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 @Entity
-public class Cidade {
+public class Cidade implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Integer id;
