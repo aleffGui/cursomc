e1defe4a93017d28bf8e261a1b563e30d238fd26
Guilherme Ferreira <alefff.guilherme@gmail.com>
2021-11-09 13:50:12 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/guilherme/cursomc/dtos/CategoriaDTO.java
+++ src/main/java/com/guilherme/cursomc/dtos/CategoriaDTO.java
@@ -1,10 +1,17 @@
 package com.guilherme.cursomc.dtos;
 
+
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.guilherme.cursomc.domain.Categoria;
 
 public class CategoriaDTO {
 
 	private Integer id;
+	@NotEmpty(message = "Preenchimento deve ser obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	
