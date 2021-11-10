42ed5c6fbfbb967433f3464e2d882071bf8d2f36
Guilherme Ferreira <alefff.guilherme@gmail.com>
2021-11-10 18:28:59 -0300

    Inserindo Pedido

--- src/main/java/com/guilherme/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/guilherme/cursomc/domain/PagamentoComCartao.java
@@ -2,8 +2,10 @@ package com.guilherme.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.guilherme.cursomc.enums.EstadoPagamento;
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/guilherme/cursomc/services/PedidoService.java
+++ src/main/java/com/guilherme/cursomc/services/PedidoService.java
@@ -1,11 +1,17 @@
 package com.guilherme.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.guilherme.cursomc.domain.ItemPedido;
+import com.guilherme.cursomc.domain.PagamentoComBoleto;
 import com.guilherme.cursomc.domain.Pedido;
+import com.guilherme.cursomc.enums.EstadoPagamento;
+import com.guilherme.cursomc.repositories.ItemPedidoRepository;
+import com.guilherme.cursomc.repositories.PagamentoRepository;
 import com.guilherme.cursomc.repositories.PedidoRepository;
 import com.guilherme.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -15,9 +21,42 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository pr;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	ItemPedidoRepository itemPedidoRepository;
 	
 	public Pedido findById(Integer id) {
 		Optional<Pedido> obj = pr.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class));
+	}
+
+
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = pr.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for(ItemPedido ip : obj.getItems()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.findById(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItems());
+		return obj;
 	}
 }
